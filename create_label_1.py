af3fa984b2af99b3e3c9fc81cdde8537b3675a41
Liu-hongwen <Programmer_23@163.com>
2017-08-08 10:31:07 +0800

    Create create_label_1.py

---
+++ create_label_1.py
@@ -0,0 +1,10 @@
+import os
+
+with open('C:/caffe-project/Ages/label_images/labels/Age_1_5.txt','w') as label_txt:
+    for root, dirs, files in os.walk('C:/caffe-project/Ages/label_images/1_5/'):
+        for file in files:
+            image_file = str(file)
+            label = image_file[:-4] + '\n'
+            label_txt.writelines(label)
+
+print "succeed"
